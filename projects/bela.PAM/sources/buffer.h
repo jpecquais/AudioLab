b994ab56adeb68b8b84bafc2fc380876101ecd66
jpecquais <48678018+jpecquais@users.noreply.github.com>
2025-04-29 15:07:19 +0200

    implementation of optimized zero latency convolution

--- projects/bela.PAM/sources/buffer.h
+++ projects/bela.PAM/sources/buffer.h
@@ -1,136 +1,103 @@
 #include <cstddef>
+#include <stdexcept>
+
 template<class T>
 class AudioBuffer
 {
 private:
-
     int m_block_size;
     int m_number_channels;
     T** m_buffer;
 
 public:
-    
-    enum class Error{
+    enum class Error {
         ok = 0,
         bad_data,
         empty_size,
         buffer_already_allocated,
         buffer_not_allocated
     };
-    
-    AudioBuffer(size_t block_size, size_t number_channel);
-    AudioBuffer(AudioBuffer& buffer);
-    AudioBuffer(AudioBuffer&& buffer);
-    ~AudioBuffer();
-    
-    T**& get();
-    inline int get_block_size(){ return m_block_size; }
-    inline int get_number_of_channels(){ return m_number_channels; }
-    
-    auto allocate_buffer() -> Error;
-    auto deallocate_buffer() -> Error;
-    
-    T* operator[](size_t idx);
 
-};
-
-template<class T>
-AudioBuffer<T>::AudioBuffer(size_t block_size, size_t number_channel):m_block_size(block_size),
-                                                                      m_number_channels(number_channel),
-                                                                      m_buffer(nullptr)
-{
-    //TODO: throw error if block_size of number_channel are == 0;
-}
-
-template<class T>
-AudioBuffer<T>::AudioBuffer(AudioBuffer& buffer) // Copy constructor
-{
-    m_block_size = buffer.get_block_size();
-    m_number_channels = buffer.get_number_of_channels();
-    allocate_buffer();
-    auto& buffer_data = buffer.get();
-    for (size_t ch = 0; ch < m_number_channels; ch++)
+    AudioBuffer(size_t block_size, size_t number_channel)
+        : m_block_size(block_size), m_number_channels(number_channel), m_buffer(nullptr)
     {
-        for (size_t i = 0; i < m_block_size; i++)
-        {
-            m_buffer[ch][i] = buffer_data[ch][i];
+        if (block_size == 0 || number_channel == 0) {
+            throw std::invalid_argument("Block size and number of channels must be greater than 0");
         }
     }
-}
 
-template<class T>
-AudioBuffer<T>::AudioBuffer(AudioBuffer&& buffer) // Move constructor
-{
-    //TODO
-}
-
-template<class T>
-AudioBuffer<T>::~AudioBuffer()
-{
-    deallocate_buffer();
-}
-
-template<class T>
-auto AudioBuffer<T>::allocate_buffer() -> Error
-{
-    if (m_block_size == 0 or m_number_channels == 0) return Error::empty_size;
-    if (!m_buffer)
+    AudioBuffer(AudioBuffer& buffer)
+        : m_block_size(buffer.get_block_size()), m_number_channels(buffer.get_number_of_channels()), m_buffer(nullptr)
     {
-        try
-        {
-            m_buffer = new float*[m_block_size];
-            for (size_t ch = 0; ch < m_number_channels; ch++)
-            {
-                m_buffer[ch] = new float[m_block_size];
-                for (size_t i = 0; i < m_block_size; i++)
-                {
-                    m_buffer[ch][i] = 0;
+        allocate_buffer();
+        auto& buffer_data = buffer.get();
+        for (size_t ch = 0; ch < m_number_channels; ch++) {
+            for (size_t i = 0; i < m_block_size; i++) {
+                m_buffer[ch][i] = buffer_data[ch][i];
+            }
+        }
+    }
+
+    AudioBuffer(AudioBuffer&& buffer)
+        : m_block_size(buffer.m_block_size), m_number_channels(buffer.m_number_channels), m_buffer(buffer.m_buffer)
+    {
+        buffer.m_buffer = nullptr;
+        buffer.m_block_size = 0;
+        buffer.m_number_channels = 0;
+    }
+
+    ~AudioBuffer() {
+        deallocate_buffer();
+    }
+
+    T**& get() {
+        return m_buffer;
+    }
+
+    inline int get_block_size() {
+        return m_block_size;
+    }
+
+    inline int get_number_of_channels() {
+        return m_number_channels;
+    }
+
+    auto allocate_buffer() -> Error {
+        if (m_block_size == 0 || m_number_channels == 0) return Error::empty_size;
+        if (!m_buffer) {
+            try {
+                m_buffer = new T*[m_number_channels];
+                for (size_t ch = 0; ch < m_number_channels; ch++) {
+                    m_buffer[ch] = new T[m_block_size]();
                 }
+            } catch (...) {
+                for (size_t ch = 0; ch < m_number_channels; ch++) {
+                    delete[] m_buffer[ch];
+                }
+                delete[] m_buffer;
+                m_buffer = nullptr;
+                return Error::bad_data;
             }
+        } else {
+            return Error::buffer_already_allocated;
         }
-        catch()
-        {
-            for (size_t _ch = 0; _ch < ch; _ch++)
-            {
-                delete[] m_buffer[_ch];
-            }
-            delete[] m_buffer;
-            return Error::bad_data;
-        }
+        return Error::ok;
     }
-    else
-    {
-        return Error::buffer_already_allocated;
-    }
-    return Error::ok;
-}
 
-template<class T>
-auto AudioBuffer<T>::deallocate_buffer() -> Error
-{
-    if (m_buffer)
-    {
-        for (size_t ch = 0; ch < m_number_channels; _ch++)
-            {
+    auto deallocate_buffer() -> Error {
+        if (m_buffer) {
+            for (size_t ch = 0; ch < m_number_channels; ch++) {
                 delete[] m_buffer[ch];
             }
-        delete[] m_buffer;
+            delete[] m_buffer;
+            m_buffer = nullptr;
+        } else {
+            return Error::buffer_not_allocated;
+        }
+        return Error::ok;
     }
-    else
-    {
-        return Error::buffer_not_allocated;
+
+    T* operator[](size_t idx) {
+        return m_buffer[idx];
     }
-    return Error::ok;
-}
-
-template<class T>
-T* AudioBuffer<T>::operator[](size_t idx)
-{
-    return m_buffer[idx];
-}
-
-template<class T>
-T**& AudioBuffer<T>::get()
-{
-    return m_buffer;
-}
+};
